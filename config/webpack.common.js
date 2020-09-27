75eb2f96b6d6908ad7322812132a64f8cdcb3028
thomas-lg <tom.legougaud@gmail.com>
2020-09-27 22:59:56 +0200

    fix(webpack): output duplicate removed

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -24,10 +24,6 @@ module.exports = ({ PLATFORM }) => {
 		resolve: {
 			extensions: ['.tsx', '.ts', '.js'],
 		},
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, '..', 'dist'),
-		},
 		plugins: [new HtmlWebpackPlugin()],
 	};
 };
